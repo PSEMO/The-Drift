dac14f0aee9f0e40de4800dbae7f5ebcdd286fd8
atayiilmaz <yiilmazataberk@gmail.com>
2023-12-08 19:15:11 +0300

    i have no idea what i'm pushing

--- main.py
+++ main.py
@@ -20,13 +20,15 @@ framerate = 150
 class car:
     pos = [300, 300]
     size = [215, 90]
-    velocity = [0, 0]
+    velocity = 0
     R = 46
     G = 21
     B = 71
 #---------------------------------
 
 Car = car()
+
+camera_offset = [0,0]
 
 #Update()
 while 1:
@@ -35,14 +37,34 @@ while 1:
 
     #resets screen
     screen.fill((100, 100, 100))
-
     
     #detect events including inputs
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    
-    pygame.draw.rect(screen, (Car.R, Car.G, Car.B), (Car.pos, Car.size))
+
+    keys = pygame.key.get_pressed()
+
+    x = 0
+    y = 0
+    a = 0
+    b = 0
+
+    if keys[pygame.K_RIGHT] == True:
+        x = 1
+    if keys[pygame.K_LEFT] == True:
+        y = 1
+    if keys[pygame.K_DOWN] == True:
+        a = 1()
+    if keys[pygame.K_UP] == True:
+        b = 1
+    Car.pos[0] = Car.pos[0] + ((x - y) * Car.velocity)
+    Car.pos[1] += (a - b) * Car.velocity
+
+    camera_offset[0] = Car.pos[0] - width / 2
+    camera_offset[1] = Car.pos[1] - height / 2
+
+    pygame.draw.rect(screen, (Car.R, Car.G, Car.B), ([ Car.pos[0] - camera_offset[0], Car.pos[1] - camera_offset[1] ], [ Car.size[0] , Car.size[1] ]))
     
     pygame.display.flip()
